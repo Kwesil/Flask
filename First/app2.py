d7027275e577f122d2231f6e5cc56f74b0160025
Kwesil <nwabikejr@gmail.com>
2025-09-02 23:54:36 +0100

    Fixed the redirect bug

---
+++ First/app2.py
@@ -0,0 +1,21 @@
+# http methods
+
+from flask import Flask, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/welcome/<name>')
+def welcome(name):
+    return 'welcome %s' % name
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('welcome', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('welcome', name=user))
+    
+if __name__ == '__main__':
+    app.run(debug=True)
